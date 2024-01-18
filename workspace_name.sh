a5aa2ba3c4999489068ac6b8c8495ed7ff12ce28
levimake <levimake10@gmail.com>
2024-01-18 22:03:28 +0530

    Update workspace_name.sh

--- workspace_name.sh
+++ workspace_name.sh
@@ -3,13 +3,18 @@
 # Pull column 10 (first word in worskpace name) till end of line from the line with an * (signifying current workspace) in the output of `wmctrl -d`
 CURRENT_WORKSPACE=$(wmctrl -d | awk '/\*/ {for (i=10; i<=NF; i++) printf "%s ", $i}')
 
+# Panel
+INFO="<txt>"
+INFO+="$CURRENT_WORKSPACE"
+INFO+="</txt>"
+
 # CSS Styling
 CSS="<css>"
 CSS+="/* ADD YOUR CSS HERE */ }" 
 CSS+="</css>"
 
-# Print the name of the current workspace encapsulated by `txt` tags for Genmon
-printf '<txt>%s</txt>' "$CURRENT_WORKSPACE"
+# Panel Print
+echo -e "${INFO}"
 
 # Add Styling
 echo -e "${CSS}"
